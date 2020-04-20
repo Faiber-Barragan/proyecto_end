ece9db6d8d8b1ea47a98fa928baeb8b21189ed02
Faiber Barragan <barragan.faiber@gmail.com>
2020-04-19 21:41:17 -0500

    Se adañe Responsive

--- PROYECTO_RAMOS/js/main.js
+++ PROYECTO_RAMOS/js/main.js
@@ -1,7 +1,24 @@
-$(document).ready(function(){
-    $('.bxslider').bxSlider({
-      mode: 'fade',
-      captions: true,
-      slideWidth: 935
-    });
+// Inicio Plugin Facebook 
+window.fbAsyncInit = function () {
+  FB.init({
+    xfbml: true,
+    version: 'v6.0'
   });
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Inicio pluging slider
+$(document).ready(function () {
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 935
+  });
+});
